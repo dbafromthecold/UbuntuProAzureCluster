1f1b340a434d19d74142cb1bb19e43620bde8c5a
Andrew Pruski <dbafromthecold@gmail.com>
2021-12-01 11:50:12 +0000

    Update 3. Create Availability Group.ps1

--- 3. Create Availability Group.ps1
+++ 3. Create Availability Group.ps1
@@ -155,24 +155,32 @@ mssql-cli -S ap-server-01 -U sa -P Testing1122 -Q "SELECT [group_name], [node_na
 sudo crm configure property stonith-enabled=false
 
 
+# jump into the crm shell
+sudo crm
 
-# create the AG resource
-sudo crm configure primitive ag1_cluster \
-   ocf:mssql:ag \
-   params ag_name="ag1" \
-   meta failure-timeout=60s \
-   op start timeout=60s \
-   op stop timeout=60s \
-   op promote timeout=60s \
-   op demote timeout=10s \
-   op monitor timeout=60s interval=10s \
-   op monitor timeout=60s on-fail=demote  interval=11s role="Master" \
-   op monitor timeout=60s interval=12s role="Slave" \
-   op notify timeout=60s
 
-sudo crm configure ms ms-ag1 ag1_cluster \
-   meta master-max="1" master-node-max="1" clone-max="3" \
-   clone-node-max="1" notify="true"
+# enter the following commands to create the AG resource
+configure
+
+
+primitive ag1_cluster \
+ ocf:mssql:ag \
+params ag_name="ag1" \
+meta failure-timeout=60s \
+op start timeout=60s \
+op stop timeout=60s \
+op promote timeout=60s \
+op demote timeout=10s \
+op monitor timeout=60s interval=10s \
+op monitor timeout=60s on-fail=demote  interval=11s role="Master" \
+op monitor timeout=60s interval=12s role="Slave" \
+op notify timeout=60s
+
+ms ms-ag1 ag1_cluster \
+meta master-max="1" master-node-max="1" clone-max="3" \
+clone-node-max="1" notify="true"
+
+commit
 
 
 
